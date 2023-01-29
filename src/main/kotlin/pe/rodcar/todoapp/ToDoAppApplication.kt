06f1acf11628fcc149b66bf2fb6ebbe24ffae3d0
rodcar <ivanrod12@hotmail.com>
2023-01-29 17:20:46 -0500

    commit inicial

---
+++ src/main/kotlin/pe/rodcar/todoapp/ToDoAppApplication.kt
@@ -0,0 +1,12 @@
+package pe.rodcar.todoapp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ToDoAppApplication
+
+fun main(args: Array<String>) {
+	runApplication<ToDoAppApplication>(*args)
+	println("Hello World")
+}
---
+++ src/test/kotlin/pe/rodcar/todoapp/ToDoAppApplicationTests.kt
@@ -0,0 +1,13 @@
+package pe.rodcar.todoapp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ToDoAppApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
